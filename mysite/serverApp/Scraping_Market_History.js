9870a1e4ac5d514a8d10352639e185e6ddf8d10f
KBobovskiy <kbobovskiy@yandex.ru>
2018-04-10 06:45:19 +0300

    add marker history scraper

---
+++ mysite/serverApp/Scraping_Market_History.js
@@ -0,0 +1,109 @@
+/**
+* Use to Scraping market history from https://the-tale.org/shop/market-history
+* @module Scraping_Market_History
+*/
+const request = require('request');
+const mysql = require('mysql');
+const HTMLParser = require('fast-html-parser');
+const login_info = require("./login_info");
+
+/*
+*   const block
+*/
+const debugOn = login_info.debugOn;
+const mysql_Host = login_info.mysql_Host;
+const mysql_User = login_info.mysql_User;
+const mysql_Password = login_info.mysql_Password;
+
+const apiClient = login_info.apiClient;
+const apiClientValue = login_info.apiClientValue;
+
+let cookies = login_info.getCookieFromString('');
+
+var csrftoken = cookies.csrftoken;
+var sessionid = cookies.sessionid;
+
+// вывод отладочной информации
+function debugPrint(message, dontPrint) {
+  if (debugOn === true && !dontPrint) {
+    console.log(message);
+  }
+}
+
+
+function requestShop(err, res) {
+  if(err){
+      console.log("requestShop: it did not work: " + err)
+  } else {
+    //printRequestStatus(res);
+    //console.log(res.headers);
+    if (err == null) {
+      console.log(res.req.path);
+
+      var con = mysql.createConnection({ host: mysql_Host, user: mysql_User, password: mysql_Password });
+      con.connect(function(err) {
+        if (err) {throw err};
+        //console.log("Connected!");
+      });
+
+      var tzoffset = (new Date()).getTimezoneOffset() * 60000; //offset in milliseconds
+      var root = HTMLParser.parse(res.body);
+      var table = root.querySelector('.table');
+      var rows = table.childNodes[3];
+
+      for (var trIndx = 0; trIndx<rows.childNodes.length; trIndx++) {
+        var row = rows.childNodes[trIndx];
+        if (row.tagName) {
+          var cardName = row.childNodes[1].childNodes[1].childNodes[0].rawText.trim();
+          var cardPrice = row.childNodes[3].childNodes[0].rawText.trim();
+          var card_description = row.childNodes[1].childNodes[1].rawAttrs.split('title')[1].split('"')[1];
+          var card_class = row.childNodes[1].childNodes[1].classNames[0];
+          var cardDealDate = row.childNodes[5].childNodes[0].rawAttrs.trim();
+          cardDealDate = cardDealDate.split('data-timestamp=');
+          cardDealDate = cardDealDate[1];
+          while (cardDealDate.includes('"')) {
+            cardDealDate = cardDealDate.replace('"','');
+          }
+          cardDealDate = parseInt(cardDealDate); //data-timestamp
+          cardDealDate = new Date(cardDealDate*1000 - tzoffset);
+          cardDealDate = cardDealDate.toISOString().slice(0, 19);
+
+          if (trIndx == 1) {
+              console.log('rows.childNodes = '+rows.childNodes.length + ' first date: ' + cardDealDate);
+          }
+
+          var queryString = "INSERT INTO thetale.market_history VALUES ("
+            +"'"+cardDealDate+"','"+cardName+"',"+cardPrice+",1,'"+card_description+"','"+card_class+"')";
+          debugPrint(queryString,1);
+          try {
+            con.query(queryString, function (err, result, fields) { /*if (err) throw err; console.log(result);*/ });
+          }
+          catch (err) {
+            //console.log(err);
+          }
+        }
+      }
+      con.end();
+      con = null;
+    }
+  }
+}
+
+function getMarketHistoryFromPage(pageNumber) {
+  var csrfmiddlewaretoken = csrftoken;
+  var cookieString = 'sessionid='+sessionid+'; csrftoken='+csrfmiddlewaretoken;
+  debugPrint(cookieString);
+  console.log(cookieString);
+  var apiURL = 'https://the-tale.org/shop/market-history?page='+pageNumber;
+  request({
+    method: "GET",
+    headers: { 'Cookie': cookieString},
+    url: apiURL,
+    form: {'csrfmiddlewaretoken':csrfmiddlewaretoken}
+  },requestShop);
+  if (pageNumber<3) {setTimeout(function() {getMarketHistoryFromPage(pageNumber+1);},2000);}
+}
+
+setTimeout(function() {getMarketHistoryFromPage(1);},2000);
+
+console.log('EoF');
