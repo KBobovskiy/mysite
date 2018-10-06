987f6869579ebace27fbe26debda38baa725e265
KBobovskiy <kbobovskiy@yandex.ru>
2018-10-07 00:40:28 +0300

    added villages list

--- mysite/Travian/login.js
+++ mysite/Travian/login.js
@@ -5,8 +5,8 @@ var sleep = require('sleep-promise');
 puppeteer.defaultArgs({ headless: false });
 
 async function start(loginInfo) {
-  var minSleepTimeInSec = 5;
-  var maxSleepTimeInSec = 15;
+  var minSleepTimeInSec = 3;
+  var maxSleepTimeInSec = 10;
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.setViewport({ width: 1920, height: 1080 })
@@ -45,8 +45,8 @@ async function start(loginInfo) {
 
 /**Scraping storage capacity and current resourses in it */
 async function ScrapDorf1Page(page) {
-  var minSleepTimeInSec = 5;
-  var maxSleepTimeInSec = 15;
+  var minSleepTimeInSec = 3;
+  var maxSleepTimeInSec = 10;
   await sleep(getRandomMS(minSleepTimeInSec, maxSleepTimeInSec));
   await page.goto('https://tx3.travian.ru/dorf1.php');
 
@@ -114,72 +114,73 @@ async function ScrapDorf1Page(page) {
   villageFields = {};
   buildingHouses = {};
 
+
+  var villageList = await page.evaluate(() => {
+    var villageLinkTemplateSelector = '#sidebarBoxVillagelist > div.sidebarBoxInnerBox > div.innerBox.content > ul';
+    var villageList = new Array;
+
+
+    var allVillage = $('#sidebarBoxVillagelist > div.sidebarBoxInnerBox > div.innerBox.content > ul');
+
+    for (var i = 0; i < allVillage[0].children.length; i++) {
+      console.log(i);
+      var villageName = allVillage[0].children[i].innerText.trim();
+      var villageLink = allVillage[0].children[i].children[0].href;
+      villageName = villageName.split('\n');
+      var coordinates = villageName[2];
+      coordinates = coordinates.replace('(', '').replace(')', '');
+      coordinates = coordinates.split('|')
+      villageList.push({ villageLink: villageLink, villageName: villageName[0], coordinats: coordinates[0] + '|' + coordinates[1] });
+    }
+    return villageList;
+  })
+
   /*
-    var villageList = await page.evaluate(() => {
-      var villageLinkTemplateSelector = '#sidebarBoxVillagelist > div.sidebarBoxInnerBox > div.innerBox.content > ul > li:nth-child(villageNumber) > a';
-      var villageNameTemplateSelector = '#sidebarBoxVillagelist > div.sidebarBoxInnerBox > div.innerBox.content > ul > li:nth-child(villageNumber) > a > div';
-      var i = 1;
-      var villageList = new Array;
-      while (i > 0) {
-        var linkSelector = villageLinkTemplateSelector.replace('villageNumber', i);
-        var villageLink = $(linkSelector).attr('href');
-  
-        var nameSelector = villageNameTemplateSelector.replace('villageNumber', i);
-        var villageName = document.querySelector(nameSelector).textContent.trim();
-  
-        console.log(villageLink);
-        console.log(villageName);
-        villageList.push({ villageLink: villageLink, villageName: villageName, coordinats: '' });
-        i++;
-      }
-      return villageList;
-    })
-  
-    var villageFields = await page.evaluate(() => {
-      var villageFieldsTemplateSelector = '#rx > area:nth-child(fieldNumber)';
-      var i = 1;
-      var fieldsList = new Array;
-      while (i <= 18) {
-        var fieldSelector = villageFieldsTemplateSelector.replace('villageNumber', i);
-        var villageLink = $(fieldSelector).attr('alt').trim();
-        var fieldLvl = villageLink.replace(/[^0-9]+/g, '');
-        var fieldName = villageLink.replace(/[0-9]+/g, '');
-  
-        fieldsList.push({
-          id: i,
-          level: fieldLvl,
-          name: fieldName
-        });
-  
-        console.log(fieldName);
-        console.log(fieldLvl);
-        console.log('********');
-        i++;
-      }
-      return fieldsList;
-    })
-  
-    var buildingHouses = await page.evaluate(() => {
-      var buildingHouseNameTemplateSelector = '#content > div.boxes.buildingList > div.boxes-contents.cf > ul > li > div.name';
-      var buildingHouseLevelTemplateSelector = '#content > div.boxes.buildingList > div.boxes-contents.cf > ul > li > div.name > span.lvl';
-      var buildingHouseDurationTemplateSelector = '#content > div.boxes.buildingList > div.boxes-contents.cf > ul > li > div.buildDuration';
-  
-      var buildingHousesList = new Array;
-  
-      var name = $(buildingHouseNameTemplateSelector).textContent.trim();
-      var level = $(buildingHouseLevelTemplateSelector).textContent.trim();
-      var duration = $(buildingHouseDurationTemplateSelector).textContent.trim();
-  
-      console.log(name);
-      console.log(level);
-      console.log(duration);
+  var villageFields = await page.evaluate(() => {
+    var villageFieldsTemplateSelector = '#rx > area:nth-child(fieldNumber)';
+    var i = 1;
+    var fieldsList = new Array;
+    while (i <= 18) {
+      var fieldSelector = villageFieldsTemplateSelector.replace('villageNumber', i);
+      var villageLink = $(fieldSelector).attr('alt').trim();
+      var fieldLvl = villageLink.replace(/[^0-9]+/g, '');
+      var fieldName = villageLink.replace(/[0-9]+/g, '');
+   
+      fieldsList.push({
+        id: i,
+        level: fieldLvl,
+        name: fieldName
+      });
+   
+      console.log(fieldName);
+      console.log(fieldLvl);
       console.log('********');
-  
-      buildingHousesList.push({ name: name, level: level, duration: duration });
-      return buildingHousesList;
-    })
-  
-    */
+      i++;
+    }
+    return fieldsList;
+  })
+   
+  var buildingHouses = await page.evaluate(() => {
+    var buildingHouseNameTemplateSelector = '#content > div.boxes.buildingList > div.boxes-contents.cf > ul > li > div.name';
+    var buildingHouseLevelTemplateSelector = '#content > div.boxes.buildingList > div.boxes-contents.cf > ul > li > div.name > span.lvl';
+    var buildingHouseDurationTemplateSelector = '#content > div.boxes.buildingList > div.boxes-contents.cf > ul > li > div.buildDuration';
+   
+    var buildingHousesList = new Array;
+   
+    var name = $(buildingHouseNameTemplateSelector).textContent.trim();
+    var level = $(buildingHouseLevelTemplateSelector).textContent.trim();
+    var duration = $(buildingHouseDurationTemplateSelector).textContent.trim();
+   
+    console.log(name);
+    console.log(level);
+    console.log(duration);
+    console.log('********');
+   
+    buildingHousesList.push({ name: name, level: level, duration: duration });
+    return buildingHousesList;
+  })
+   
+  */
 
   //console.log(storageInfo);
   villageFields = {};
